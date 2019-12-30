9d1cd5965fcb02c8b6a9e0a75ebac3d3ef60a6af
Le Duy Anh <lenduya@gmail.com>
2019-12-30 23:11:34 +0100

    Make ViewHolders clickable

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -40,13 +41,24 @@ class CrimeListFragment: Fragment() {
     }
 
 
-    private inner class CrimeHolder(view: View): RecyclerView.ViewHolder(view) {
+    private inner class CrimeHolder(view: View)
+        : RecyclerView.ViewHolder(view), View.OnClickListener {
 
+        private lateinit var crime: Crime
         private val titleTextView = itemView.findViewById<TextView>(R.id.crime_title)
         private val dateTextView = itemView.findViewById<TextView>(R.id.crime_date)
         private val solvedImageView = itemView.findViewById<ImageView>(R.id.crime_solved)
 
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            Toast.makeText(context, "${crime.title} pressed!", Toast.LENGTH_SHORT).show()
+        }
+
         fun bind(crime: Crime) {
+            this.crime = crime
             titleTextView.text = crime.title
             dateTextView.text = crime.date.toString()
             solvedImageView.visibility = if(crime.isSolved) {
@@ -60,7 +72,6 @@ class CrimeListFragment: Fragment() {
     private inner class CrimeAdapter(var crimes: List<Crime>)
         : RecyclerView.Adapter<CrimeHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
-
             val view =
                 layoutInflater.inflate(R.layout.list_item_crime, parent, false)
             return CrimeHolder(view)
