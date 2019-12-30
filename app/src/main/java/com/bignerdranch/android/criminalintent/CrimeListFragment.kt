b7f5bc99ff69b51b5f51b632594856789453917d
Le Duy Anh <lenduya@gmail.com>
2019-12-30 18:30:12 +0100

    Implement onCreateView() and create CrimeHolder

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -1,7 +1,14 @@
 package com.bignerdranch.android.criminalintent
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class CrimeListFragment: Fragment() {
@@ -9,5 +16,39 @@ class CrimeListFragment: Fragment() {
     private lateinit var crimeRecyclerView: RecyclerView
     private val crimeListViewModel: CrimeListViewModel by lazy {
         ViewModelProviders.of(this).get(crimeListViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_crime_list, container, false)
+
+        crimeRecyclerView = view.findViewById(R.id.crime_recycler_view)
+        crimeRecyclerView.layoutManager = LinearLayoutManager(context)
+        //TODO: adapter
+
+        return view
+    }
+
+
+
+    private inner class CrimeHolder(view: View): RecyclerView.ViewHolder(view) {
+
+        private val titleTextView = itemView.findViewById<TextView>(R.id.crime_title)
+        private val dateTextView = itemView.findViewById<TextView>(R.id.crime_date)
+        private val solvedImageView = itemView.findViewById<ImageView>(R.id.crime_solved)
+
+        fun bind(crime: Crime) {
+            titleTextView.text = crime.title
+            dateTextView.text = crime.date.toString()
+            solvedImageView.visibility = if(crime.isSolved) {
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
+        }
     }
 }
