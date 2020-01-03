6f3a94bae34d4243b5702668aedac736673cefa3
Le Duy Anh <lenduya@gmail.com>
2020-01-04 00:07:42 +0100

    Add companion functions

--- app/src/main/java/com/bignerdranch/android/criminalintent/Time.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Time.kt
@@ -1,7 +1,30 @@
 package com.bignerdranch.android.criminalintent
 
 import java.io.Serializable
+import java.util.*
 
 class Time(
     private val hour: Int,
-    private val minute: Int): Serializable
+    private val minute: Int): Serializable {
+
+    companion object {
+        fun dateToTime(date: Date): Time {
+            val calendar = Calendar.getInstance()
+            calendar.time = date
+
+            return Time(
+                calendar.get(Calendar.HOUR_OF_DAY),
+                calendar.get(Calendar.MINUTE))
+        }
+
+        fun addTimeToDate(date: Date, time: Time): Date {
+            val calendar = Calendar.getInstance()
+            calendar.time = date
+
+            calendar.set(Calendar.HOUR_OF_DAY, time.hour)
+            calendar.set(Calendar.MINUTE, time.minute)
+
+            return calendar.time
+        }
+    }
+}
